6f19b1dab5eba32d8aa8cca01cee72091af8fad9
Shoiti <shoiti.tokunaga@gmail.com>
2020-01-15 09:57:10 -0300

    1_15_2020

---
+++ Desafio01_Csharp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Desafio01_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String marca = "Hyundai", modelo = "HB20", placa = "22222", cor = "Preto";
+            bool isligado = false, isAcelerar=false, isFrear=false, isPintar=false;
+            int qtdabastecer, combustivel = 100, velocidade = 0, preco = 2;
+
+            Veiculo meucarro = new Veiculo(marca, modelo, placa, cor, isligado, combustivel, velocidade, preco);
+            while(true)
+            {
+                Console.WriteLine("Ligar/Desligar o veículo?");
+                String escolha = Console.ReadLine();
+                switch (escolha)
+                {
+                    case "Ligar":
+                        meucarro.ligar();
+                        break;
+                    case "Desligar":
+                        meucarro.desligar();
+                        break;
+                }
+                if (meucarro.isLigado == true)
+                {
+                    Console.WriteLine("Deseja pintar seu veículo?(Sim/Não)");
+                    String escolhapintar = Console.ReadLine();
+                    switch (escolhapintar)
+                    {
+                        case "Sim":
+                            Console.WriteLine("Qual cor?");
+                            String newcor = Console.ReadLine();
+                            meucarro.pintar(newcor);
+                            break;
+                    }
+                    Console.WriteLine("Deseja acelerar?(Sim/Não)");
+                    String escolhaacelerar = Console.ReadLine();
+                    switch (escolhaacelerar)
+                    {
+                        case "Sim":
+                            meucarro.acelerar();
+                            break;
+                    }
+                    if (meucarro.Velocidade > 0)
+                    {
+                        Console.WriteLine("Deseja frear?(Sim/Não)");
+                        String escolhafrear = Console.ReadLine();
+                        switch (escolhafrear)
+                        {
+                            case "Sim":
+                                meucarro.frear();
+                                break;
+                        }
+                    }
+                    Console.WriteLine("Deseja abastecer?(Sim/Não)");
+                    String escolhaabastecer = Console.ReadLine();
+                    switch (escolhaabastecer)
+                    {
+                        case "Sim":
+                            Console.WriteLine("Quantos litros deseja abastecer?");
+                            int qtdlitros = Convert.ToInt32(Console.ReadLine());
+                            meucarro.abastecer(qtdlitros);
+                            break;
+                    }
+                }
+                Console.WriteLine("___________________________________________________");
+                Console.WriteLine("MEU VEÍCULO:");
+                Console.WriteLine("Marca: "+meucarro.Marca);
+                Console.WriteLine("Modelo: "+meucarro.Modelo);
+                Console.WriteLine("Placa: "+meucarro.Placa);
+                Console.WriteLine("Cor: "+meucarro.Cor);
+                if (meucarro.isLigado == true) 
+                {
+                    Console.WriteLine("O veículo está ligado com velocidade de " + meucarro.Velocidade + "Km/h.");
+                }
+                else
+                {
+                    Console.WriteLine("O veículo está desligado.");
+                }
+                    Console.WriteLine("Está com "+meucarro.LitrosCombustivel+" litros de combustível.");
+                Console.WriteLine("___________________________________________________");
+            }
+        }
+    }
+}
---
+++ Desafio01_Csharp/Veiculo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio01_Csharp
+{
+    class Veiculo
+    {
+        private String _Marca, _Modelo, _Placa, _Cor;
+        private bool _isLigado;
+        private int _LitrosCombustivel, _Velocidade;
+        private double _Preco;
+
+        public Veiculo(String marca, String modelo, String placa, String cor, bool isligado, int combustivel, int velocidade, double preco)
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Placa = placa;
+            this.Cor = cor;
+            this.isLigado = isligado;
+            this.LitrosCombustivel = combustivel;
+            this.Velocidade = velocidade;
+            this.Preco = preco;
+        }
+        public String Cor { get; set; }
+        public bool isLigado { get; set; }
+        public int LitrosCombustivel { get; set; }
+        public int Velocidade { get; set; }
+        public double Preco { get; }
+        public string Marca { get; }
+        public string Modelo { get; }
+        public string Placa { get; }
+
+        public void acelerar()
+        {
+            if (this.LitrosCombustivel > 0)
+            {
+                this.Velocidade += 20;
+                this.LitrosCombustivel--;
+            }
+            else
+            {
+                Console.WriteLine("Você está sem combustível e não pode acelerar");
+            }
+        }
+        public void abastecer(int qtdLitros)
+        {
+            if (qtdLitros >= 0)
+            {
+                this.LitrosCombustivel += qtdLitros;
+                if (this.LitrosCombustivel > 100)
+                {
+                    this.LitrosCombustivel -= qtdLitros;
+                    int sobra = 100 - this.LitrosCombustivel;
+                    Console.WriteLine("Você abasteceu somente " + sobra + " Litros de combustível, pois excedeu o limite de 100 Litros.");
+                    this.LitrosCombustivel = 100;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Você não pode abastecer essa quantidade.");
+            }
+        }
+        public void frear()
+        {
+            if (this.Velocidade > 0)
+            {
+                this.Velocidade -= 10;
+            }
+        }
+        public void pintar(String cor)
+        {
+            this.Cor = cor;
+        }
+        public void ligar()
+        {
+            if (this.isLigado == false)
+            {
+                this.isLigado = true;
+                this.Velocidade = 20;
+            }
+        }
+        public void desligar()
+        {
+            this.isLigado = false;
+        }
+    }
+}
